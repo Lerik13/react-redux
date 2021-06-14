1ab9a05a24d6d141586ebef8860fa93f2c80e858
Valeria <lerik13@gmail.com>
2021-06-14 12:36:17 -0600

    Show Alert if during creating post, the title is empty

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux'
-import {createPost} from '../redux/actions'
+import {createPost, showAlert} from '../redux/actions'
+import { Alert } from './Alert';
 
 class PostForm extends React.Component {
 	constructor(props) {
@@ -17,7 +18,7 @@ class PostForm extends React.Component {
 		const {title} = this.state
 
 		if (!title.trim()) {
-			return 
+			return this.props.showAlert('Title of post could not be empty')
 		}
 
 		const newPost = {
@@ -39,6 +40,8 @@ class PostForm extends React.Component {
 	render() {
 		return (
 			<form onSubmit={this.submitHandler}>
+				{this.props.alert && <Alert text={this.props.alert} />}
+
 				<h1>Post Form </h1>
 				<div className="mb-3">
 					<label htmlFor="title" className="form-label">Title Post</label>
@@ -58,6 +61,11 @@ class PostForm extends React.Component {
 }
 
 const mapDispatchToProps = {
-	createPost
+	createPost, showAlert
 }
-export default connect(null, mapDispatchToProps)(PostForm)
+
+const mapStateToProps = state => ({
+	alert: state.app.alert
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostForm)
